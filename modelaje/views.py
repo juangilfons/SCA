fc4141cfeef86a96b42c93d45e22a323c0edacfc
Juan Gil <juangilfons@gmail.com>
2024-12-06 13:37:37 -0300

    Fixed bug in get_opcion view

--- modelaje/views.py
+++ modelaje/views.py
@@ -73,7 +73,8 @@ def create_opcion(request):
     serializer = OpcionDecisionSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['DELETE'])
