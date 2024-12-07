3a78f57fdc040efb7d68a3f6e7f21175e5b959ab
Juan Gil <juangilfons@gmail.com>
2024-12-07 20:24:04 -0300

    Configured CORS in settings.py

--- SCA/settings.py
+++ SCA/settings.py
@@ -27,6 +27,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  # Origen del frontend local
+]
+
 
 # Application definition
 
@@ -39,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "modelaje.apps.ModelajeConfig",
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
