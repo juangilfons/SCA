090f3f363bb8a6cf77a7754f71b3bff1441589b9
Juan Gil <juangilfons@gmail.com>
2024-12-09 00:11:22 -0300

    fixed get_important_areas api_view

--- modelaje/views.py
+++ modelaje/views.py
@@ -14,6 +14,7 @@ def get_areas(request):
     serializer = AreaDecisionSerializer(areas, many=True)
     return Response(serializer.data)
 
+@api_view(['GET'])
 def get_important_areas(request):
     areas = AreaComparacion.objects.filter(is_important=True)
     serializer = AreaComparacionSerializer(areas, many=True)
