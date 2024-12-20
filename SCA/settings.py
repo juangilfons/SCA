7483d882d48f3ef0ef9ef3af21dda83f4488ef0e
Juan Gil <juangilfons@gmail.com>
2024-12-20 01:51:33 -0300

    Debug True

--- SCA/settings.py
+++ SCA/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*&q#4d=cj6f$dj70f%pe8!+j)8l=i@6yv-!r=(7x29&%wu3f7&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
