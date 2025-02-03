98abf578cad506d29a4aff12078b9010c1b43e71
Juan Gil <juangilfons@gmail.com>
2025-02-03 17:56:12 -0500

    debug allowed cors origin

--- SCA/settings.py
+++ SCA/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
-    "https://desarrollosca.netlify.app/"
+    "https://desarrollosca.netlify.app"
 ]
 
 
